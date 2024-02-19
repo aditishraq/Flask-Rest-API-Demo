8e069cdc831190cc051104455da3d98c1de56cb1
Adit <44678491+aditishraq@users.noreply.github.com>
2024-02-19 11:26:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, abort  # Import abort for error handling
+from flask import Flask, request, jsonify, render_template, abort  
 from temperature import celsius_to_fahrenheit
 import config
 
@@ -13,18 +13,14 @@ def index():
 @app.route('/convert', methods=['GET'])
 def convert_temperature():
     """Convert temperature from Celsius to Fahrenheit and return JSON."""
-    # Attempt to get the 'celsius' query parameter and convert it to float
     celsius_input = request.args.get('celsius', default=None)
     
-    # Validate the input
     if celsius_input is not None:
         try:
             celsius = float(celsius_input)
         except ValueError:
-            # If conversion to float fails, return a 400 Bad Request error
             abort(400)
     else:
-        # If no input is provided, default to 0
         celsius = 0
 
     fahrenheit = celsius_to_fahrenheit(celsius)
